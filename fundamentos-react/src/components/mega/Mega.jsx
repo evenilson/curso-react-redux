4f4607989c48d940621ee91777c213ca605b8bbc
evenilson <evenilsonlp@gmail.com>
2020-10-30 00:37:32 -0300

    Atualizando desafio megasena

--- fundamentos-react/src/components/mega/Mega.jsx
+++ fundamentos-react/src/components/mega/Mega.jsx
@@ -6,7 +6,7 @@ import numeros from './geraNumeros'
 
 export default (props) => {
 
-    const [qtde, setQtde] = useState(props.qtde)
+    const [qtde, setQtde] = useState(props.qtde || 7)
 
 
     function mudarQtde(e){
@@ -19,7 +19,7 @@ export default (props) => {
         const lis = []
         for (let index = 0; index < numeros.length; index++) {
             lis.push(
-                <li>{numeros[index]}</li>
+                <li key={index}>{numeros[index]}</li>
             )
         }
         return lis
@@ -28,8 +28,11 @@ export default (props) => {
     return (
 
         <div className='mega'>
+            <h2>Mega</h2>
             <label htmlFor="passoInput">Total gerado: </label>
             <input
+                min={7}
+                max={15}
                 id="passoInput"
                 type="number"
                 value={qtde}
